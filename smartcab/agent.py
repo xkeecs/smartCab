48e30ec076f8228950f70c7ea02bca89012bfefb
Kai Xiong <xkeecs@gmail.com>
2016-07-10 00:59:24 -0400

    Initial commit

--- smartcab/agent.py
+++ smartcab/agent.py
@@ -11,10 +11,16 @@ class LearningAgent(Agent):
         self.color = 'red'  # override color
         self.planner = RoutePlanner(self.env, self)  # simple route planner to get next_waypoint
         # TODO: Initialize any additional variables here
+        self.previous_reward = 0
+        self.previous_action = None
 
     def reset(self, destination=None):
         self.planner.route_to(destination)
         # TODO: Prepare for a new trip; reset any variables here, if required
+        self.previous_reward = 0
+        self.previous_action = None
+        self.state = None
+
 
     def update(self, t):
         # Gather inputs
